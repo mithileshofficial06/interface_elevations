073c1a72358d0ffe2dec3eb72115ceb1d3ad7c6b
Mithiilesh K S <mithidec2006@gmail.com>
2026-04-03 02:28:15 +0530

    Brands Added

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Services from '@/components/Services';
 import ProjectsHorizontal from '@/components/ProjectsHorizontal';
+import Clients from '@/components/Clients';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <About />
         <Services />
         <ProjectsHorizontal />
+        <Clients />
       </main>
     </IntroAnimation>
   );
